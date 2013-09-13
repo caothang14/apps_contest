bb35d237ed2f86a117f63ca6d66a5830bdd381b2
Linh Nguyen <nguyenvanlinh.bk@gmail.com>
2013-09-14 01:43:28 +0700

    Initial commit

---
+++ app/controllers/maps_controller.rb
@@ -0,0 +1,9 @@
+class MapsController < ApplicationController
+  def index
+    #return array of companies info
+  end
+
+  def get_position
+    #binding.pry
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AppsContest::Application.config.secret_key_base = 'd167f80234793eb4e683c02ae7e0bd62d70c54c173ed22d08663b449145ecd25a034a736fd324ea2aa3a998b1b21cb02ce26ae8ab11144b2f52bea76484d585b'
