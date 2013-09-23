f51e31a0736754bc8951f31d9f7b2e0afcaf3906
Le Duc Thang <leducthang@macbook-007-no-macbook.local>
2013-09-23 16:34:50 +0700

    log in with phone number

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ActiveRecord::Base
-	has_many :stores;
+	has_many :stores
+
+	validates :phone_number, uniqueness: true
+
+	has_secure_password :validations => false
+	validates :password, length: { minimum: 6 }, :if => :check_phone_number
 	def self.from_omniauth(auth)
 	  where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
 	    user.provider = auth.provider
@@ -11,7 +16,20 @@ class User < ActiveRecord::Base
 	  end
 	end
 
+	def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.encrypt(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
 	def own_store? store
 		self.id == store.user_id
 	end
+
+	def check_phone_number
+		self.phone_number
+	end
+
 end
